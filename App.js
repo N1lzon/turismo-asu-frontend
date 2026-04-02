d68f39c027a803347fa86c92a211218d9db13430
Nilson <nilsoncasco@outlook.com>
2026-04-02 18:37:41 -0300

    Add screens, update dependencies, and configure gitignore

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import OptionsScreen from './screens/OptionsScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Tab.Navigator>
+        <Tab.Screen name="Inicio" component={HomeScreen} />
+        <Tab.Screen name="Mapa" component={MapScreen} />
+        <Tab.Screen name="Opciones" component={OptionsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
